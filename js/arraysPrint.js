d6d9487d364786373ad3fb5c02e23e59c38b8d70
Magdum <magdunium@gmail.com>
2019-09-04 19:31:27 +0200

    listed all elements

--- js/arraysPrint.js
+++ js/arraysPrint.js
@@ -3,7 +3,7 @@ function printAll(arrayName)
     var emptyArray = [];
     for(var i=0; i < arrayName.length; i++)
     {
-        emptyArray.push(arrayName[i] + "<li>");
+        emptyArray.push("<li>" + arrayName[i]);
         document.getElementById("arrayPrint").innerHTML = "<ul>" + emptyArray + "</ul>";
     };
 };
